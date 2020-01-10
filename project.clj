97f3810d1a303f84f119f0187c90e867e402ea52
Eric Scott <eric.scott@acm.org>
2020-01-10 11:39:41 -0800

    Upgrading clojure to v 1.10.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [ont-app/sparql-endpoint "0.1.1-SNAPSHOT"]
                  [ont-app/igraph "0.1.4-SNAPSHOT"]
                  [ont-app/vocabulary "0.1.0-SNAPSHOT"]
