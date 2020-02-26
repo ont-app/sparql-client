07918a927f38c71945fb7ab5077feacafd51fd30
Eric Scott <eric.scott@acm.org>
2020-02-26 09:42:53 -0800

    Updating project.clj. Codox.

--- project.clj
+++ project.clj
@@ -4,15 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [selmer "1.11.8"]
-                 ;;
+                 ;; 3rd party 
+                 [selmer "1.12.18"]
                  [com.taoensso/timbre "4.10.0"]
-                 [org.clojure/tools.reader "1.2.2"]
-                 ;; ... see https://github.com/ptaoussanis/timbre/issues/263
-                 ;; included per docs in slf4j-timbre...
-                 ;;[org.slf4j/log4j-over-slf4j "1.7.14"]
-                 ;;[org.slf4j/jul-to-slf4j "1.7.14"]
-                 ;;[org.slf4j/jcl-over-slf4j "1.7.14"]
+                 [org.clojure/tools.reader "1.3.2"]
                  ;; ont-app
                  [ont-app/graph-log "0.1.0"]
                  [ont-app/igraph "0.1.4"]
@@ -21,4 +16,6 @@
                  
                  ]
   :target-path "target/%s"
+  :plugins [[lein-codox "0.10.6"]]
+  :codox {:output-path "doc"}
   :profiles {:uberjar {:aot :all}})
